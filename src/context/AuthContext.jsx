916b49c815a287f07aa225dfedf410ed11be80e3
PedroSuarez1402 <pssuarez1402@gmail.com>
2024-11-25 12:19:08 -0500

    arreglos en la sesion

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,17 +1,42 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import clienteAxios from "../config/axios";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    const fetchUserData = async() => {
+        const token = localStorage.getItem("token");
+        if (!token){
+            setLoading(false);
+            return;
+        }
+        try {
+            const response = await clienteAxios.get("/session-data");
+            console.log("peticion de sesion:", response.data)
+            setUser(response.data.user)
+        } catch (error) {
+            console.error("Error al obtener los datos del usuario de la sesion:", error)
+            setUser(null);
+
+        }finally{
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        
+        fetchUserData();
+    }, [])
     const login = (userData) => {
         setUser(userData);
+        fetchUserData();
     }
 
     return (
-        <AuthContext.Provider value={{user, login}}>
+        <AuthContext.Provider value={{user, login, loading}}>
             {children}
         </AuthContext.Provider>
     )
--- src/views/request/Requests.jsx
+++ src/views/request/Requests.jsx
@@ -1,90 +1,120 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import clienteAxios from '../../config/axios';
-import { CButton, CCard, CCardBody, CCardFooter, CCardHeader, CCardText, CCardTitle, CCol, CRow, CSpinner } from '@coreui/react';
-import { AuthContext } from '../../context/AuthContext';
-import FormRequests from './FormRequests';
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import clienteAxios from "../../config/axios";
+import { CButton, CCard, CCardBody, CCardFooter, CCardHeader, CCardText, CCardTitle, CCol, CRow, CSpinner,
+} from "@coreui/react";
+//import { AuthContext } from "../../context/AuthContext";
+import FormRequests from "./FormRequests";
 
 const Requests = () => {
-    const [requests, setRequests] = useState([]);
-    const {user} = useContext(AuthContext);
-    const [loading, setLoading] = useState(false);
-    const [isCreating, setIsCreating] = useState(false)
-    const [error, setError] = useState(null);
+  const [requests, setRequests] = useState([]);
+  //const { user, loading } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isCreating, setIsCreating] = useState(false);
+  const [error, setError] = useState(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchRequest = async() => {
-            
-            try {
-                const token = localStorage.getItem('token');
-                if(!token){
-                    navigate('/login');
-                    return;
-                }
-                
-                const response = await clienteAxios.get(`/solicitudes/${user.userId}/${user.rolId}`);
-                setRequests(response.data.solicitudes);
-            } catch (error) {
-                setError('Error al cargar las solicitudes')
-            }
-        };
-        fetchRequest();
-    },[user, navigate])
+  const fetchRequest = async () => {
+    setLoading(true)
+    try {
+        const userResponse = await clienteAxios.get("/session-data");
+        const userData = userResponse.data.user;
 
-    const handleOpenModal = () => setIsCreating(true);
-    const handleCloseModal = () => setIsCreating(false);
-    return (
-        <CRow className='g-3'>
-            <CCol xs={12}>
-                <CCard>
-                    <CCardHeader className='d-flex justify-content-between align-items-center'>
-                        <strong>Solicitudes</strong>
-                        <CButton color='primary' onClick={handleOpenModal}>Crear Solicitud</CButton>
-                    </CCardHeader>
-                    <CCardBody>
-                    {loading ? (
-                        <CSpinner color='primary' />
-                    ) : error ? (
-                        <p className='text-danger'>{error}</p>
-                    ) : requests.length === 0 ? (
-                        <p>No hay solicitudes disponibles</p>
-                    ) : (
-                        <CRow className='g-3'>
-                        {requests.map((request) => (
-                            <CCol xs={12} sm={6} md={4} lg={3} key={request}>
-                                <CCard className='h-100'>
-                                <CCardHeader>
-                                    <h6 className='mb-2'>Solicitud #{request.id}</h6>
-                                </CCardHeader>
-                                <CCardBody>
-                                    <CCardTitle>{request.name}</CCardTitle>
-                                    <CCardText>
-                                        {request.description} <br/>
-                                        
-                                    </CCardText>
-                                </CCardBody>
-                                <CCardFooter>
-                                    <CButton color='primary' variant='outline' className='w-100'>
-                                        Ver
-                                    </CButton>
-                                </CCardFooter>
-                                </CCard>
-                            </CCol>
-                        ))}
-                            
-                        </CRow>
-                    )}
-                        
-                    </CCardBody>
-                </CCard>
-            </CCol>
-            {isCreating && <FormRequests visible={isCreating} onClose={handleCloseModal} />}
-        </CRow>
+        if(!userData){
+            throw new Error("No hay sesion activa");
+        }
+        setUser(userData);
 
-    )
-}
+        const response = await clienteAxios.get(`/solicitudes/${userData.id}/${userData.rol}`)
+        setRequests(response.data.solicitudes)
+    } catch (error) {
+        console.error("Error al obtener la sesión o solicitudes:", error);
+        setError("Error al cargar datos. Por favor, inicia sesión nuevamente.");
+        navigate("/login"); // Redirige si no hay sesión activa
+    } finally {
+        setLoading(false);
+    }
+  };
 
-export default Requests
+  useEffect(() => {
+    /* const fetchRequest = async () => {
+      if (!user || loading) return;
+
+      try {
+        const response = await clienteAxios.get(
+          `/solicitudes/${user.id}/${user.rol}`
+        );
+        setRequests(response.data.solicitudes);
+      } catch (error) {
+        console.error("error al cargar las solicitudes:", error);
+        setError("Error al cargar las solicitudes");
+      }
+    };
+    */
+    fetchRequest(); 
+  }, []);
+
+  const handleOpenModal = () => setIsCreating(true);
+  const handleCloseModal = () => setIsCreating(false);
+  return loading ? (
+    <CSpinner color="primary" />
+  ) : (
+    <CRow className="g-3">
+      <CCol xs={12}>
+        <CCard>
+          <CCardHeader className="d-flex justify-content-between align-items-center">
+            <strong>Solicitudes</strong>
+            <CButton color="primary" onClick={handleOpenModal}>
+              Crear Solicitud
+            </CButton>
+          </CCardHeader>
+          <CCardBody>
+            {loading ? (
+              <CSpinner color="primary" />
+            ) : error ? (
+              <p className="text-danger">{error}</p>
+            ) : requests.length === 0 ? (
+              <p>No hay solicitudes disponibles</p>
+            ) : (
+              <CRow className="g-3">
+                {requests.map((request) => (
+                  <CCol xs={12} sm={6} md={4} lg={3} key={request.id}>
+                    <CCard className="h-100">
+                      <CCardHeader>
+                        <h6 className="mb-2">Solicitud #{request.id}</h6>
+                      </CCardHeader>
+                      <CCardBody>
+                        <CCardTitle>{request.name}</CCardTitle>
+                        <CCardText>
+                          {request.description} <br />
+                        </CCardText>
+                      </CCardBody>
+                      <CCardFooter>
+                        <CButton
+                          color="primary"
+                          variant="outline"
+                          className="w-100"
+                        >
+                          Ver
+                        </CButton>
+                      </CCardFooter>
+                    </CCard>
+                  </CCol>
+                ))}
+              </CRow>
+            )}
+          </CCardBody>
+        </CCard>
+      </CCol>
+      {isCreating && (
+        <FormRequests visible={isCreating} onClose={handleCloseModal} />
+      )}
+    </CRow>
+  );
+};
+
+export default Requests;
